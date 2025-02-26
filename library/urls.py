e471d9bfe58a8476f29045d9f22cda2584effd4a
Edgar Tadevosyan <tadevosyanedgar22@gmail.com>
2025-02-26 22:52:55 +0400

    feat: Add reservation notifications and update borrowing logic

--- library/urls.py
+++ library/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('book/<slug:book_slug>/', views.BookDetail.as_view(), name='book_detail'),
     path('deletebook/<slug:slug>/', views.DeleteBook.as_view(), name='delete_book'),
     path('borrow_book/<int:book_id>', views.borrow_book, name='borrow_book'),
+    path('reserve_book/<int:book_id>', views.reserve_book, name='reserve_book'),
     path('return_book/<int:book_id>', views.return_book, name='return_book'),
     path('borrowed-books/', views.borrowed_books, name='borrowed_books'),
     path('categories/', views.categories, name='categories'),
